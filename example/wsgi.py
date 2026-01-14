942008782b9344d42f39d62bcf781c3cf4dd2cd5
Franco <frfantusatti@protonmail.com>
2026-01-14 06:45:30 -0300

    edit wsgi

--- example/wsgi.py
+++ example/wsgi.py
@@ -2,7 +2,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
 
 application = get_wsgi_application()
+
+app = application
